ba3ee18bbf799834aa3c7578514aeae31ffc844a
oscar <oscarfung06@gmail.com>
2020-07-16 11:25:03 +0800

    First commit-2

--- app/src/main/java/com/demo/myapplication/MainActivity.java
+++ app/src/main/java/com/demo/myapplication/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
-
+// This is Oscar's project
 public class MainActivity extends AppCompatActivity {
 
     @Override
